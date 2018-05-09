eb9c32496695c820951938d2f1f4fc4e8df39d91
Alex Yin <34431568+alexyin2@users.noreply.github.com>
2018-05-09 15:52:43 +0800

    Add files via upload

---
+++ t_test_exercise.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+# We'll use the dataset 'advertisement_clicks.csv'
+df = pd.read_csv('/Users/alex/Desktop/DeepLearningPrerequisites/Git_Code&Dataset/machine_learning_examples/ab_testing/advertisement_clicks.csv')
+A = df[df.iloc[:, 0] == 'A']
+B = df[df.iloc[:, 0] == 'B']
+mean_a = A.iloc[:, 1].mean()
+mean_b = B.iloc[:, 1].mean()
+var_a = A.iloc[:, 1].var(ddof=1)  # Normalized by N-1 by default. Here we just type it out to make it clearer.
+var_b = B.iloc[:, 1].var(ddof=1)
+
+# Calculate t statistic
+N = 1000
+t = (mean_a - mean_b) / np.sqrt(var_a / N + var_b / N)
+df = 2 * N - 2
+
+# Calculate p
+p = stats.t.cdf(t, df=df)  # one-sided t test p-value, H1: mean_a < mean_b
+
+t2, p2 = stats.ttest_ind(A.iloc[:, 1], B.iloc[:, 1], equal_var=False)  # two-sided t test 
